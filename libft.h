10f946c469aea5cce6e9758aea701b069b244ed3
omar <omarfelk20@gmail.com>
2023-10-17 18:45:35 +0200

    add_back_no

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:16:15 by omfelk            #+#    #+#             */
-/*   Updated: 2023/10/16 16:38:57 by omfelk           ###   ########.fr       */
+/*   Updated: 2023/10/17 18:22:52 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,10 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
-//void	ft_lstadd_front(t_list **lst, t_list *new);
-////int		ft_lstsize(t_list *lst);
-//t_list	*ft_lstlast(t_list *lst);
-//void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 //void	ft_lstdelone(t_list *lst, void (*del)(void*));
 //void	ft_lstclear(t_list **lst, void (*del)(void*));
 //void	ft_lstiter(t_list *lst, void (*f)(void *));
